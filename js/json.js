a8783e7ee13b7d20148343d778548ef18622fe48
Bapreet <75432662+Bapreet@users.noreply.github.com>
2024-03-20 11:49:46 -0400

    Done Lab 3

--- js/json.js
+++ js/json.js
@@ -6,7 +6,7 @@ const section = document.querySelector("section");
 async function populate() {
     // Introducing JavaScript Object Notation (JSON): https://json.org/
     // STEP 4: Store the URL of a JSON file in a variable */
-    const requestURL = "https://smccrindle.github.io/comp1073module10/js/i-scream.json";
+    const requestURL = "https://raw.githubusercontent.com/eldritch-quinn/comp-1073-lesson-10/main/js/i-scream.json";
     // STEP 5: Use the new URL to create a new request object
     const request = new Request(requestURL);
     // STEP 6: Make a network request with the fetch() function, which returns a Response object
@@ -45,9 +45,12 @@ function showTopFlavors(jsonObj) {
         let h2 = document.createElement("h2");
         let image = document.createElement("img");
         let ul = document.createElement("ul");
+        let para = document.createElement("p");
 
         // STEP 10f: Set the textContent property for each of the above elements (except the UL), based on the JSON content
         h2.textContent = topFlavors[i]["name"];
+        para.textContent = "Calories: " + topFlavors[i]["calories"];
+
         image.setAttribute("src", `images/${topFlavors[i]["image"]}`);
        // console.log(image);
         // STEP 10g: Build a loop for the ingredients array in the JSON
@@ -62,6 +65,7 @@ function showTopFlavors(jsonObj) {
         article.appendChild(h2);
         article.appendChild(image);
         article.appendChild(ul);
+        article.appendChild(para);
         console.log(article);
         // STEP 10i: Append each complete ARTICLE element to the SECTION element
         section.appendChild(article);
